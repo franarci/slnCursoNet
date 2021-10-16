002e8ca24a4e542cf5374ce4e2e7217aa1fffdfe
franarci <fran.arciniega96@gmail.com>
2021-10-15 21:51:28 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioCosto { get; set; }
+        public double Margen { get; set; }
+        public double IVA { get; set; }
+        public decimal PrecioBruto { get; }
+
+    }
+}
