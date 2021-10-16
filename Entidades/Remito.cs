bb119c43714d05fa1eb9b0097a555ff3f1faa9ce
franarci <fran.arciniega96@gmail.com>
2021-10-15 23:24:13 -0300

    constructor en Remito

--- Entidades/Remito.cs
+++ Entidades/Remito.cs
@@ -17,5 +17,20 @@ namespace Entidades
         public string Detalle { get; set; }
         public DateTime FechaEntrega { get; set; }
         public decimal Total { get; set; }
+
+        public Remito(string numero, DateTime fecha, string cliente, string dir, string condIva, string condVenta, string detalle, DateTime fechaEntrega, decimal total)
+        {
+            Numero = numero;
+            Fecha = fecha;
+            Cliente = cliente;
+            Direccion = dir;
+            CondicionIVA = condIva;
+            CondicionVenta = condVenta;
+            Detalle = detalle;
+            FechaEntrega = fechaEntrega;
+            Total = total;
+           }
     }
+
+    
 }
