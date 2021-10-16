562cbd8a9f2f20e495780407053c331d3527ec0d
franarci <fran.arciniega96@gmail.com>
2021-10-15 23:29:43 -0300

    constructor en ClienteIndividuo

--- Entidades/ClienteIndividuo.cs
+++ Entidades/ClienteIndividuo.cs
@@ -14,5 +14,15 @@ namespace Entidades
         public string Email { get; set; }
         public string Telefono { get; set; }
         public string Direccion { get; set; }
+
+        public ClienteIndividuo(string nombre, string apellido, string cuit, string email, string telefono, string direccion)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            CUIT = cuit;
+            Email = email;
+            Telefono = telefono;
+            Direccion = direccion;
+        }
     }
 }
