05e8e4ea462b9561054d94ce42e33e3e0efd873b
franarci <fran.arciniega96@gmail.com>
2021-10-17 18:13:23 -0300

    Agrego herencia en clase Vendedor

--- Entidades/Models/Vendedor.cs
+++ Entidades/Models/Vendedor.cs
@@ -3,26 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades.BaseModels;
 
 namespace Entidades
 {
-    public class Vendedor
+    public class Vendedor : Persona
     {
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
         public string DNI { get; set; }
-        public string Email { get; set; }
-        public string Telefono { get; set; }
-        public string Direccion { get; set; }
 
-        public Vendedor(string nombre, string apellido, string dni, string email, string telefono, string direccion)
+        public Vendedor(string nombre, string apellido, string dni, string email, string telefono, string direccion) : base(nombre, apellido, email, telefono, direccion)
         {
-            Nombre = nombre;
-            Apellido = apellido;
             DNI = dni;
-            Email = email;
-            Telefono = telefono;
-            Direccion = direccion;
         }
     }
 }
