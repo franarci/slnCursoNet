0626aa4253efaa07b6a70536463268d53e199549
franarci <fran.arciniega96@gmail.com>
2021-10-17 18:16:42 -0300

    Agrego clase abstracta DocumentoComercial

---
+++ Entidades/BaseModels/DocumentoComercial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.BaseModels
+{
+    public abstract class DocumentoComercial
+    {
+        public string Numero { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Cliente { get; set; }
+        public string Direccion { get; set; }
+        public string CondicionIVA { get; set; }
+        public string CondicionVenta { get; set; }
+        public string Detalle { get; set; }
+        public decimal Total { get; set; }
+
+        public DocumentoComercial(string tipo, string numero, DateTime fecha, string cliente, string dir, string condIva, string condVenta, string detalle, decimal total)
+        {
+            Numero = numero;
+            Fecha = fecha;
+            Cliente = cliente;
+            Direccion = dir;
+            CondicionIVA = condIva;
+            CondicionVenta = condVenta;
+            Detalle = detalle;
+            Total = total;
+        }
+    }
+}
